7c2e02933a3084b1c571c7d24bb43292b9a93af9
pratikash <pratikashpanda.1234@gmail.com>
2023-05-13 09:18:09 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all rights reserved</p>
+      </div>
+      <div>
+        <h5>Follow us</h5>
+        <div>
+          <a href="https://youtube.com" target={"blank"}>
+            YouTube
+          </a>
+          <a href="https://instagram.com/thepratikashpanda" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/pratikashpanda" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer
---
+++ src/components/Home.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import vg from "../assets/2.webp";
+import {
+  AiFillGoogleCircle,
+  AiFillAmazonCircle,
+  AiFillYoutube,
+  AiFillInstagram,
+} from "react-icons/ai";
+
+const Home = () => {
+  return (
+    <>
+      <div className="home" id="home">
+        <main>
+          <h1>TechStar</h1>
+          <p>Solutions to all your problems</p>
+        </main>
+      </div>
+      <div className="home2">
+        <img src={vg} alt="graphics" />
+        <div>
+          <p>
+            We are your one and only solution to the tech problems you face
+            every day. We are leading tech company whose aim is to increase the
+            problem solving ability in children.
+          </p>
+        </div>
+      </div>
+
+      <div className="home3" id="about">
+        <div>
+          <h1>Who we are?</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet
+            natus quos odio voluptatem, cumque expedita, quod sunt repellendus
+            nemo optio dicta exercitationem quisquam error culpa tenetur rerum
+            labore eos officiis explicabo odit quas reiciendis. Ea deserunt
+            quia, consequuntur minus nobis, odit molestias, optio obcaecati
+            nesciunt eligendi saepe asperiores eos magni!
+          </p>
+        </div>
+      </div>
+
+      <div className="home4" id="brands">
+        <div>
+          <h1>Brands</h1>
+          <article>
+            <div style={{ animationDelay: "0.3s" }}>
+              <AiFillGoogleCircle />
+              <p>Google</p>
+            </div>
+            <div style={{ animationDelay: "0.5s" }}>
+              <AiFillAmazonCircle />
+              <p>Amazon</p>
+            </div>
+            <div style={{ animationDelay: "0.7s" }}>
+              <AiFillYoutube />
+              <p>YouTube</p>
+            </div>
+            <div style={{ animationDelay: "0.9s" }}>
+              <AiFillInstagram />
+              <p>Instagram</p>
+            </div>
+          </article>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
